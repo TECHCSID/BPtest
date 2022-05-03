f91ac655e9631a3b52340b65865fe0d54a277270
TECHCSID <30695095+TECHCSID@users.noreply.github.com>
2022-05-03 16:37:23 +0200

    Update SPEEDTESTSECIBV3.ps1

--- SPEEDTESTSECIBV3.ps1
+++ SPEEDTESTSECIBV3.ps1
@@ -7,13 +7,10 @@ $output = "C:\Windows\Temp\rgsupv\speedtest.exe"
 }
 
 $Speedtest = cmd /c "C:\Windows\Temp\rgsupv\speedtest.exe -f json --accept-gdpr --accept-license"
-
 $Download = $Speedtest.split(':')[9]
 $resultD = $Download.split(',')[0]
-
 $Upload = $Speedtest.split(':')[13]
 $resultU = $Upload.split(',')[0]
-
 $ResultUtemp = ($resultD / 1000000)
 $XDownload = $ResultUtemp * 8
 $res1 = [math]::round($XDownload,2)
@@ -22,13 +19,11 @@ $ResultUtempB = ($resultU / 1000000)
 $Xupload = $ResultUtempB * 8
 $res2 = [math]::round($Xupload,2)
 
-$PourRG = "Download= $res1 Mbps / Upload= $res2 Mbps"
+$PourRG = "Download: $res1 Mb/s Upload: $res2 Mb/s"
 
 $array = $speedtest -split "isp"
 $array2 = $array -split "interface"
-
 $trim = $array2[1]
-
 $words = $trim.Split(":")[1]
 $resultISP = $words.split(',')[0]
 
@@ -45,4 +40,4 @@ If (-not (Test-Path $registryPath)) {New-Item -Path HKlm:\Software\SECIB -Name D
 
 New-ItemProperty -Path $registryPath -Name 'Débit' -Value $PourRG -PropertyType STRING -Force | Out-Null
 New-ItemProperty -Path $registryPath -Name 'FAI' -Value $resultISP -PropertyType STRING -Force | Out-Null
-write-output "$resultISP $PourRG Ping: $resultLAms"
+write-output "Provider:$resultISP $PourRG Ping: $resultLA ms"
